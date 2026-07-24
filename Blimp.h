4885e6bb161c47d7655e4a8f167c0c65b3b969bd
Daniyar <danijarfairushin@gmail.com>
2026-07-24 18:37:26 +0300

    fix: include local AP_Arming.h instead of upstream
    Our AP_Arming.h defines AP_Arming_Blimp which inherits from
    AP_Arming. The upstream include didn't bring in our derived class.

--- Blimp.h
+++ Blimp.h
@@ -19,7 +19,7 @@
 #include <AP_Math/AP_Math.h>
 #include <AP_Vehicle/AP_Vehicle.h>
 #include <AP_RCMapper/AP_RCMapper.h>
-#include <AP_Arming/AP_Arming.h>
+#include "AP_Arming.h"
 
 #include "defines.h"
 #include "config.h"
