754d442cdcd6a3417a3f464d05881ca2140ab2c7
Daniyar <danijarfairushin@gmail.com>
2026-07-27 00:15:28 +0300

    feat: updating param version

--- Parameters.cpp
+++ Parameters.cpp
@@ -27,7 +27,7 @@ const AP_Param::Info Blimp::var_info[] = {
     // @DisplayName: Eeprom format version number
     // @Description: This value is incremented when changes are made to the eeprom format
     // @User: Advanced
-    GSCALAR(format_version, "FORMAT_VERSION",   0),
+    GSCALAR(format_version, "FORMAT_VERSION",   1),
 
     // @Param: SYSID_THISMAV
     // @DisplayName: MAVLink system ID of this vehicle
